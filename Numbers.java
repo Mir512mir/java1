c64a3ea5b8b5a7263b71703eec3d753cae7e9ec1
Mir512mir <123588015+Mir512mir@users.noreply.github.com>
2023-02-13 00:21:36 +0100

    Add files via upload

---
+++ Numbers.java
@@ -0,0 +1,38 @@
+public class Numbers
+{
+
+    public static void main(String[] args)
+    {
+       // int a16=333;
+        // 16 -> 10
+       // System.out.println("Result 333 (16 -> 10) = " + a16);
+
+       // System.out.println("Result (16 ->10) = " + (3*Math.pow(16,2) + 3*Math.pow(16,1) + 3*Math.pow(16,0)));
+       // System.out.println("Result 200345 = " + (2*100000+0*10000+0*1000+3*100+4*10+5*1));
+       int b=637;
+       // System.out.println("Result 637=" +b);
+       // System.out.println("Result (10 -> 16) =" +Integer.toHexString(b));
+       // System.out.println("Result (16 -> 10) =" + (2*Math.pow(16,2)+7*Math.pow(16,1)+13*Math.pow(16,0)));
+      //  System.out.println("Result (10 -> 2) =" + Integer.toBinaryString(b));
+       // 10 -> 3
+        System.out.println(637/3); //212
+        System.out.println(637%3); //1
+
+        System.out.println(212/3); //70
+        System.out.println(212%3); //2
+
+        System.out.println(70/3); //23
+        System.out.println(70%3); //1
+
+        System.out.println(23/3); //7
+        System.out.println(23%3); //2
+
+        System.out.println(7/3);  //2
+        System.out.println(7%3);  //1
+
+        System.out.println(2/3);  //0
+        System.out.println(2%3);  //2
+        System.out.println("Result 637 (10 -> 3) =212121");
+        System.out.println("Result 11100111 (2 -> 10)="+(1*Math.pow(2,7)+1*Math.pow(2,6)+1*Math.pow(2,5)+0*Math.pow(2,4)+0*Math.pow(2,3)+1*Math.pow(2,2)+1*Math.pow(2,1)+1*Math.pow(2,0)));
+}
+}
